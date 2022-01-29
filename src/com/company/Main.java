a957e70e741d761faf53fc4e0c38dcf7ecfda837
Sorbon Imomnazarov <isorbon@gmail.com>
2022-01-29 18:05:09 +0200

    The Basic Structure is complete

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -4,7 +4,9 @@ public class Main {
     public static void main(String[] args) {
 
         SolarSystem solar = new SolarSystem();
-        
+        solar.sun=1;
+        solar.Moon=2;
+        solar.Planets=3;
 
     }
 }
